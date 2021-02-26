8a04983e52149a8622a1cfa951b9cb21ffd73a42
helter daniel <helterdaniel@hotmail.com>
2021-02-26 19:20:09 -0300

    modelo de cada pais

---
+++ Coronavirus/Models/CovidCountry.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coronavirus.Models
+{
+    public class CovidCountry
+    {
+        public int updated { get; set; }
+        public string country { get; set; }
+        public Countryinfo countryInfo { get; set; }
+        public int cases { get; set; }
+        public int todayCases { get; set; }
+        public int deaths { get; set; }
+        public int todayDeaths { get; set; }
+        public int recovered { get; set; }
+        public int todayRecovered { get; set; }
+        public int active { get; set; }
+        public int critical { get; set; }
+
+        public int tests { get; set; }
+        public int population { get; set; }
+        public int continent { get; set; }
+    }
+
+    public class Countryinfo
+    {
+        public string iso2 { get; set; }
+        public string iso3 { get; set; }
+        public string flag { get; set; }
+    }
+}
