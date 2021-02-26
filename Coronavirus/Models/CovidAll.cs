200a5b9d924e811fa50dc473fc1ae3b9c5c8f359
helter daniel <helterdaniel@hotmail.com>
2021-02-26 19:23:42 -0300

    modelos otimizados

--- Coronavirus/Models/CovidAll.cs
+++ Coronavirus/Models/CovidAll.cs
@@ -4,19 +4,8 @@ using System.Text;
 
 namespace Coronavirus.Models
 {
-    public class CovidAll
+    public class CovidAll : Content
     {
-        public int updated { get; set; } // TimeStamp data da atualização
-        public int cases { get; set; } // total de casos
-        public int deaths { get; set; } // total de mortos
-        public int recovered { get; set; } // total de recuperados
-
-        public int active { get; set; } // ativos
-        public int critical { get; set; } // casos críticos
-
-        public int tests { get; set; } // total de teste?
-
-        public int population { get; set; } // total população
         public int affectedCountries { get; set; } // países afetados
     }
 }
--- Coronavirus/Models/CovidCountry.cs
+++ Coronavirus/Models/CovidCountry.cs
@@ -4,22 +4,15 @@ using System.Text;
 
 namespace Coronavirus.Models
 {
-    public class CovidCountry
+    public class CovidCountry : Content
     {
-        public int updated { get; set; }
         public string country { get; set; }
         public Countryinfo countryInfo { get; set; }
-        public int cases { get; set; }
-        public int todayCases { get; set; }
-        public int deaths { get; set; }
-        public int todayDeaths { get; set; }
-        public int recovered { get; set; }
-        public int todayRecovered { get; set; }
-        public int active { get; set; }
-        public int critical { get; set; }
 
-        public int tests { get; set; }
-        public int population { get; set; }
+        public int todayCases { get; set; }
+
+        public int todayDeaths { get; set; }
+
         public int continent { get; set; }
     }
 
