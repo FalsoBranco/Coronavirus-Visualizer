6867521a389583eb75d0d2f97eb35c1c6fdd6623
helter daniel <helterdaniel@hotmail.com>
2021-02-26 19:16:55 -0300

    modelo total

---
+++ Coronavirus/Models/CovidAll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Coronavirus.Models
+{
+    public class CovidAll
+    {
+        public int updated { get; set; } // TimeStamp data da atualização
+        public int cases { get; set; } // total de casos
+        public int deaths { get; set; } // total de mortos
+        public int recovered { get; set; } // total de recuperados
+
+        public int active { get; set; } // ativos
+        public int critical { get; set; } // casos críticos
+
+        public int tests { get; set; } // total de teste?
+
+        public int population { get; set; } // total população
+        public int affectedCountries { get; set; } // países afetados
+    }
+}
