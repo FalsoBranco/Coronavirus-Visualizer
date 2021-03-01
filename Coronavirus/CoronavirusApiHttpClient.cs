edead5e3148c485a03dfffd2b56c68659b8e2113
helter daniel <helterdaniel@hotmail.com>
2021-03-01 19:32:03 -0300

    requisição genérica

---
+++ Coronavirus/CoronavirusApiHttpClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Coronavirus
+{
+    public class CoronavirusApiHttpClient : HttpClient
+    {
+        public CoronavirusApiHttpClient()
+        {
+            BaseAddress = new Uri("https://disease.sh/v3");
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint)
+        {
+            HttpResponseMessage responseMessage = await GetAsync(endpoint);
+            string jsonResponse = await responseMessage.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(jsonResponse);
+        }
+    }
+}
